03ed7a5344e9d4ae6d8a1a285e0e03cee249b8a5
Richy Rose <richardwaters866@gmail.com>
2026-09-26 11:40:05 +0100

    Coming soon background colour change

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const page = () => {
     return (
-        <div className="w-full h-screen flex justify-centre items-centre bg-black p-4">
+        <div className="w-full h-screen flex justify-centre items-centre bg-black p-4 text-centre">
             <h1 className="text-[#ff729f] text-4xl font-bold">COMING SOON</h1>
         </div>
     )
